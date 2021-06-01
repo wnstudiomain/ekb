002d4e5a6fc463a6c5d0e4feb334b65c62f17e2b
Nikolay Khanis <hn@wnstudio.ru>
2021-06-02 02:15:21 +0500

    add menu-mobile

---
+++ .top_content_multilevel.menu.php
@@ -0,0 +1,39 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Каталог",
+		"/catalog/",
+		Array(),
+		Array("NOT_VISIBLE"=>"Y", "CLASS"=>"catalog wide_menu"),
+		""
+	),
+	Array(
+		"Ресторанам и кофейням",
+		"/services/restoranam-i-kofejnyam/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Кофемашина в офис",
+		"/services/kofemachina-v-ofis/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Сервисный центр",
+		"/services/remont_diagnostika_kofemashin/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Акции месяца",
+		"/actions/",
+		Array(),
+		Array(),
+		""
+	)
+);
+?>
