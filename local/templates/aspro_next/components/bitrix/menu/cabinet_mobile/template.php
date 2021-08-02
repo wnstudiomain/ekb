ae59d8e6147ac21fdcfdcd968fd5b3b90613fcc5
Nikolay Khanis <hn@wnstudio.ru>
2021-08-02 16:36:00 +0500

    change url for mobile login

--- local/templates/aspro_next/components/bitrix/menu/cabinet_mobile/template.php
+++ local/templates/aspro_next/components/bitrix/menu/cabinet_mobile/template.php
@@ -6,7 +6,8 @@
 <div class="menu middle">
 	<ul>
 		<li<?=(CNext::isPersonalPage() ? ' class="selected"' : '')?>>
-			<?$link = str_replace('/', SITE_DIR, \Bitrix\Main\Config\Option::get('aspro.next', 'PERSONAL_PAGE_URL', SITE_DIR.'personal/'));?>
+                <?//$link = str_replace('/', SITE_DIR, \Bitrix\Main\Config\Option::get('aspro.next', 'PERSONAL_PAGE_URL', SITE_DIR.'personal/'));?>
+            <?$link = '/personal/';?>
 			<a rel="nofollow" class="dark-color<?=($bParent ? ' parent' : '')?>" href="<?=$link;?>">
 				<?=CNext::showIconSvg("cabinet", SITE_TEMPLATE_PATH."/images/svg/".($USER->IsAuthorized() ? 'User' : 'Lock')."_black.svg");?>
 				<span><?=GetMessage('CABINET_LINK2')?></span>
