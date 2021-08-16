e890328a9fdbeb446c91f82c9e7689940c406bb0
Nikolay Khanis <hn@wnstudio.ru>
2021-08-16 12:33:04 +0500

    add mail order init

--- local/templates/aspro_next/php_iterface/init.php
+++ local/templates/aspro_next/php_iterface/init.php
@@ -1,26 +1,28 @@
 <? AddEventHandler("sale", "OnOrderNewSendEmail", "ModifyOrderSaleMails");
-	function ModifyOrderSaleMails($orderID, &$eventName, &$arFields)
-	{
-	   if(CModule::IncludeModule("sale") && CModule::IncludeModule("iblock"))
-	   {
-	 //СОСТАВ ЗАКАЗА РАЗБИРАЕМ SALE_ORDER НА ЗАПЧАСТИ
-	      $strOrderList = "";
-	      $dbBasketItems = CSaleBasket::GetList(
-	                 array("NAME" => "ASC"),
-	                 array("ORDER_ID" => $orderID),
-	                 false,
-	                 false,
-	                 array("PRODUCT_ID", "ID", "NAME", "QUANTITY", "PRICE", "CURRENCY")
-	               );
-	 while ($arProps = $dbBasketItems->Fetch())
-	  {
-  //ПЕРЕМНОЖАЕМ КОЛИЧЕСТВО НА ЦЕНУ
-      $summ = $arProps['QUANTITY'] * $arProps['PRICE'];
-	  //СОБИРАЕМ В СТРОКУ ТАБЛИЦЫ
-	       $strCustomOrderList .= "<tr><td>".$arProps['NAME']."</td><td>".$arProps['QUANTITY']."</td><td>".$arProps['PRICE']."</td><td>".$arProps['CURRENCY']."</td><td>".$summ."</td><tr>";
-	  }
-	  //ОБЪЯВЛЯЕМ ПЕРЕМЕННУЮ ДЛЯ ПИСЬМА
-	  $arFields["ORDER_TABLE_ITEMS"] = $strCustomOrderList;
+function ModifyOrderSaleMails($ID, &$eventName, &$arFields)
+{
+    if (CModule::IncludeModule("sale") && CModule::IncludeModule("iblock")) {
+        //СОСТАВ ЗАКАЗА РАЗБИРАЕМ SALE_ORDER НА ЗАПЧАСТИ
+        $strOrderList = "";
+        $dbBasketItems = CSaleBasket::GetList(array(), array("ORDER_ID" => $ID));
+        while ($arBasket = $dbBasketItems->GetNext()) {
+            $mxResult = CCatalogSku::GetProductInfo($arBasket['PRODUCT_ID']);
+            $prop_common = CIBlockElement::GetByID($arBasket['PRODUCT_ID']);
+            $ob = $prop_common->GetNextElement();
+            $prop = $ob->GetProperties();
+            if (is_array($mxResult) && !empty($mxResult['ID'])) {
+                $arProduct = CIBlockElement::GetByID($mxResult['ID'])->Fetch();;
+            } else {
+                $arProduct = CIBlockElement::GetByID($arBasket['PRODUCT_ID'])->Fetch();
+            }
+            //ПЕРЕМНОЖАЕМ КОЛИЧЕСТВО НА ЦЕНУ
+            $summ = $arBasket['QUANTITY'] * $arBasket['PRICE'];
+            //СОБИРАЕМ В СТРОКУ ТАБЛИЦЫ
+            $strCustomOrderList .= $prop['MINIMAL_COUNT_GOODS']['VALUE'];
+        }
+        //ОБЪЯВЛЯЕМ ПЕРЕМЕННУЮ ДЛЯ ПИСЬМА
+        $arFields["ORDER_TABLE_ITEMS"] = $strCustomOrderList;
+    }
 }
-}
+
 ?>
